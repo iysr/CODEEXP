8b0a4213cf01847c992241229811216fa532bedb
Isaac Yeap Sheng Rong <isaacyeapsr@gmail.com>
2022-06-09 13:51:54 +0800

    pages added

--- codeexp/App.js
+++ codeexp/App.js
@@ -1,18 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
+import * as React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Feather } from '@expo/vector-icons'; 
+import HomeScreen from './pages/home';
+import UserScreen from './pages/user';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'You') {
+              iconName = 'user';
+            }
+
+            // You can return any component that you like here!
+            return <Feather name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'green',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="You" component={UserScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
