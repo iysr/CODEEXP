bd090b785cda2960e02b0bf9f10243dbe3ee4e7f
ash15khng <ash15khng@gmail.com>
2022-06-13 13:50:49 +0800

    working on articles

--- codeexp/pages/guide.js
+++ codeexp/pages/guide.js
@@ -1,38 +1,49 @@
-import * as React from 'react'
-import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, ScrollView, FlatList, Dimensions, TouchableOpacity } from 'react-native';
 import { Header } from './components/header'
+import { createStackNavigator } from '@react-navigation/stack';
+
+const articles = [
+  {
+    title: "How to survive in NS",
+    author: "Joshua"
+  },
+  {
+    title: "After NS - What's Next",
+    author: "Yezhou"
+  },
+  {
+    title: "5 common rifle errors",
+    author: "Isaac"
+  },
+];
 
 const Article = (props) => {
-  return <Text>{`${props.title} by ${props.author}`}</Text>
+  return (
+    <TouchableOpacity
+      onPress={() => {
+
+      }}
+    >
+      <Text>{`${props.title} by ${props.author}`}</Text>
+    </TouchableOpacity>
+  )
 }
 
 let width = Dimensions.get('window').width;
 let height = Dimensions.get('window').height;
 
-export default function UserScreen() {
+export default function GuideScreen() {
     return (
-      <View>
+      <ScrollView>
         <Header content='2 New Guides'></Header>
         <Text>Guides!</Text>
         <FlatList 
-          data={[
-            {
-              title: "How to survive in NS",
-              author: "Joshua"
-            },
-            {
-              title: "After NS - What's Next",
-              author: "Yezhou"
-            },
-            {
-              title: "5 common rifle errors",
-              author: "Isaac"
-            },
-          ]}
+          data={articles}
           renderItem={
             ({ item }) => <Article title={item.title} author={item.author}></Article>
           }
         />
-      </View>
+      </ScrollView>
     );
 }
