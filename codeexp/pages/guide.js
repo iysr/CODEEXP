5ef9cb11d183465f2f3014e0f5e3d1ae14db3080
ash15khng <ash15khng@gmail.com>
2022-06-13 12:24:38 +0800

    added some articles to guides

--- codeexp/pages/guide.js
+++ codeexp/pages/guide.js
@@ -1,10 +1,33 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const Article = (props) => {
+  return <Text>{`${props.title} by ${props.author}`}</Text>
+}
 
 export default function GuideScreen() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Guide!</Text>
+        <Text>Guides!</Text>
+        <FlatList 
+          data={[
+            {
+              title: "How to survive in NS",
+              author: "Joshua"
+            },
+            {
+              title: "After NS - What's Next",
+              author: "Yezhou"
+            },
+            {
+              title: "5 common rifle errors",
+              author: "Isaac"
+            },
+          ]}
+          renderItem={
+            ({ item }) => <Article title={item.title} author={item.author}></Article>
+          }
+        />
       </View>
     );
   }
