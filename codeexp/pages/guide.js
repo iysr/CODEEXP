8fb022f04f5a2178097aceb512a5e2aec3ab763f
ash15khng <ash15khng@gmail.com>
2022-06-13 18:19:50 +0800

    very hardcoded featured article

--- codeexp/pages/guide.js
+++ codeexp/pages/guide.js
@@ -17,7 +17,7 @@ const articles = [
   {
     title: "After NS - What's Next",
     author: "Yezhou",
-    article: "life",
+    article: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dapibus sapien lorem, sit amet tincidunt purus elementum lobortis. Ut faucibus varius mollis. Sed malesuada efficitur erat, vel vulputate eros fermentum feugiat. Phasellus sed quam placerat, interdum orci ac, sodales tellus. Aenean ultrices accumsan metus a pellentesque. Duis a ante ultricies felis ornare pulvinar. Nam quis nibh felis. Nullam sit amet mi metus. Curabitur nec pretium neque. Ut et nulla in magna vestibulum laoreet in sit amet sem. Etiam at orci dictum, fermentum lacus in, fringilla lacus. Etiam nec lectus at odio malesuada fringilla. Mauris suscipit a urna ut aliquet. Nam vulputate magna id massa cursus, quis porttitor quam lacinia. \n\nIn eget rutrum sapien, at sollicitudin odio.Phasellus quis aliquet lectus.Nam vel convallis nisi.Cras dictum nunc eget porttitor finibus.Interdum et malesuada fames ac ante ipsum primis in faucibus.Pellentesque non scelerisque eros, vitae consequat sapien.Nunc venenatis ipsum quis felis eleifend euismod.Maecenas vel venenatis tortor.Fusce pellentesque lacus ullamcorper ex efficitur ultricies.Sed ut enim dignissim, bibendum nisl a, euismod lacus.Vestibulum vel odio massa.Aenean sodales mi non porttitor blandit.In efficitur, lectus ut porttitor feugiat, metus felis fringilla tortor, ut feugiat dolor eros eu magna.Vestibulum non elit risus. \n\nNam eu ultricies sem.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Integer ornare, est a luctus vehicula, augue elit placerat leo, pharetra scelerisque quam quam nec velit.Ut commodo dignissim congue.Ut eros nibh, tempus ut erat eu, gravida congue dolor.Aliquam in consectetur justo.Nullam pellentesque turpis at massa tempus, eu posuere felis aliquet.Nam tristique ex dui, in laoreet ligula efficitur sed.\n\nNulla molestie facilisis tortor.Curabitur fermentum non turpis nec aliquam.Proin sed iaculis sem.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin molestie dapibus condimentum.Sed hendrerit, enim sed tempus tempor, mauris dui consequat libero, ac lacinia nisl eros eu mi.Aliquam congue libero a commodo consectetur.Nunc sagittis tortor nec placerat scelerisque.Morbi in accumsan ipsum.Duis maximus leo eu gravida aliquam.Vivamus ut orci eget mauris maximus pretium nec ut diam.Nulla facilisis nunc sed leo mattis, non lacinia orci ornare.Praesent et egestas turpis, at tempor quam.Phasellus eu neque vitae metus varius pellentesque vitae nec ligula.Nullam vestibulum convallis mi.\n\nDuis varius metus dictum, auctor sapien in, maximus elit.Phasellus rutrum est et velit fringilla, ac feugiat magna suscipit.Nulla convallis ante a lorem pretium, in finibus ligula interdum.Vestibulum id risus quam.Proin venenatis lacus ac metus malesuada dignissim.Vestibulum in erat mi.Praesent ultricies, odio quis hendrerit posuere, elit arcu gravida quam, a ultrices ligula felis nec ipsum.Nunc dictum pretium justo in imperdiet.Duis eget odio sit amet turpis efficitur efficitur.Aenean non ipsum bibendum, tempus erat ac, posuere tellus.Sed eget ante maximus, tincidunt justo a, facilisis augue.",
     image: "https://miro.medium.com/max/1400/0*17oIVIxMTwLhztBI",
     txtclr: '#fff',
   },
@@ -33,12 +33,12 @@ const articles = [
 const ArticleScreen = ({ route }) => {
   const {title, author, article, image} = route.params;
   let img={uri:image}
-  return (<View style={{marginLeft:20,marginRight:20,marginTop:20}}>
+  return (<ScrollView style={{marginLeft:20,marginRight:20,marginTop:20}}>
     <Text style={{fontSize:30,borderBottomColor:'#000',borderStyle:'solid',borderBottomWidth:2}}>{title}</Text>
     <Text style={{fontSize:20,marginTop:5,marginBottom:20}}>By: {author}</Text>
     <Image source={img} resizeMode="cover" style={{width:width-40,borderRadius:25,height:200,marginBottom:20}}></Image>
     <Text style={{fontSize:15}}>{article}</Text>
-  </View>)
+  </ScrollView>)
 }
 
 const Article = (props) => {
--- codeexp/pages/home.js
+++ codeexp/pages/home.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
 import { Header } from './components/header'
 import { Activity } from './components/activity'
 import { H1 } from './components/textlevels'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 let width = Dimensions.get('window').width;
 let height = Dimensions.get('window').height;
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
     return (
       <ScrollView>
         <Header content={'Welcome,'+'\n'+'Carlos Lee Ye Zhou!'}></Header>
@@ -15,6 +16,22 @@ export default function HomeScreen() {
         <Activity image='https://c1.wallpaperflare.com/preview/21/2/561/asphalt-ground-fixed-asphalt-pavement.jpg' txtclr='#fff' name='Physical Training' location='Parade Square' time='8:00'></Activity>
         <Activity image='https://media.istockphoto.com/photos/beautiful-background-of-black-and-red-picture-id1307317595?b=1&k=20&m=1307317595&s=170667a&w=0&h=hVqXq24qtxfR-7zK9Q6MP9_jew4dOqBoe86RhigenBw=' txtclr='#fff' name='Tactical Training' location='Firing Range' time='11:00'></Activity>
         <Activity image='https://wallpaperaccess.com/full/3813543.jpg' txtclr='#fff' name='Network Security' location='Computer Lab 2' time='15:00'></Activity>
+        <H1 content="Featured Articles"></H1>
+        <TouchableOpacity onPress={
+          () => navigation.navigate("Guides", {
+            screen: "ArticleScreen", 
+            params: {
+              title: "How to Survive in NS",
+              author: "Joshua",
+              article: "get good lol",
+              image: "https://probonoaustralia.com.au/wp-content/uploads/2016/11/survive.jpg",
+              txtclr: '#fff',
+            },
+            initial: false,
+          })
+        }>
+          <Activity image='https://wallpaperaccess.com/full/3813543.jpg' txtclr='#fff' name="How to Survive in NS"></Activity>
+        </TouchableOpacity>
       </ScrollView>
     );
 }
